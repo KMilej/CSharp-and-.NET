eb58be3698a7a8d224f3a59785917b8367bfd957
KM <127880662+KMilej@users.noreply.github.com>
2024-09-24 10:33:32 +0100

    easy task to call a method
    method call

---
+++ Method Class Task/Method Class Task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths maths = new Maths();
+            maths.Operation(10, 5);   // we call a method on instance maths
+
+            Console.ReadLine(); // keep program on
+        }
+    }
+
+}
