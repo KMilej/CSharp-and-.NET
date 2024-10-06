e511854ea8cea00aeb825dbef49f25750039180f
KM <127880662+KMilej@users.noreply.github.com>
2024-10-06 22:37:30 +0100

    Add files via upload

---
+++ OperatorsTasks/OperatorsTasks/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsTasks
+{
+    internal class Company
+    {
+        public List<Employee> Employees { get; set; }
+
+        public static Company operator +(Company company, Employee employee) // in operator method we should use static, company is a returned type by this method, and after add we retrun company 
+        {
+            company.Employees.Add(employee);
+            return company;
+        }
+        public static Company operator -(Company company, Employee employee)
+        {
+            company.Employees.Remove(employee);
+            return company;
+        }
+
+    }
+}
---
+++ OperatorsTasks/OperatorsTasks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company();          // we make new object of type Company
+            company.Employees = new List<Employee>();  // we make a inisialize a list of employees like a new empty list.
+
+            Employee employee = new Employee();  // we make new employee
+            employee.FirstName = "KAmil";    // we set up him a name
+
+            company = company + employee;   //we add this worker to employee list and we use overloaded operators
+
+            Console.WriteLine("List of Employteees: " + company.Employees.Count); // we see how many workers are in list 
+
+
+            if (company.Employees.Count > 0) // if workers is more then 0 then show this value in index [0]
+            {
+                Employee firstEmployee = company.Employees[0]; // we get from list 1 worker and we show 1st employee from list
+                Console.WriteLine("First Person from list : " + firstEmployee.FirstName);
+            }
+            else
+            {
+                Console.WriteLine("List of Employees are empty."); // if list is empty thne we say it 
+            }
+
+                company = company - employee;       // - 1 employee and we use - overload operator
+
+                Console.WriteLine("List of Employteees: " + company.Employees.Count);  // show how many employee is inside list
+                Console.ReadLine();
+        }
+    }
+}
