88a67b6348e0b02f4828d5296e7ea67e4d1a14b5
KM <127880662+KMilej@users.noreply.github.com>
2024-10-07 14:03:20 +0100

    Add files via upload

---
+++ Parsing Enums/Parsing Enums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool invalid = false;
+            while (!invalid)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the current day of the week");
+                    string dayinput = Console.ReadLine();
+
+                    TestEnum.DaysOfTheWeek day = (TestEnum.DaysOfTheWeek)Enum.Parse(typeof(TestEnum.DaysOfTheWeek), dayinput, true);
+
+                    Console.WriteLine("Have a nice " + day);
+                    Console.ReadLine();
+                    invalid = true;
+                }
+                catch (ArgumentException ex) 
+                {
+                    Console.WriteLine("please enter an actual dayof the week");
+                }
+            }
+
+        }
+    }
+}
