fcac04296c54818d01ceb9de546033deebeccaa7
KM <127880662+KMilej@users.noreply.github.com>
2024-11-14 22:04:47 +0000

    Add files via upload

---
+++ DataTimeTask/DataTimeTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTimeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print the currnt data and time to the console
+            Console.WriteLine("the current time is: " + DateTime.Now);
+            // ask the user for a number
+            Console.WriteLine("\nEnter a number to see what the time will be in the many hours: ");
+            float answer = float.Parse(Console.ReadLine());
+            //print what time it will be in the amount of hours the user input
+            Console.WriteLine("{0} + {1} hours(s) = {2}", DateTime.Now, answer, DateTime.Now.AddHours(answer));
+
+            Console.ReadLine(); 
+        }
+    }
+}
