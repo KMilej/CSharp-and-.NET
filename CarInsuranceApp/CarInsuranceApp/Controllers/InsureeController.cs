27771966b16a0e0d3e6f7e392846e469871db747
KM <kmilejside@gmail.com>
2025-08-04 21:51:28 +0100

    add

--- CarInsuranceApp/CarInsuranceApp/Controllers/InsureeController.cs
+++ CarInsuranceApp/CarInsuranceApp/Controllers/InsureeController.cs
@@ -46,17 +46,19 @@ namespace CarInsuranceApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insurees insurees)
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType")] Insurees insuree)
         {
             if (ModelState.IsValid)
             {
-                db.Insurees.Add(insurees);
+                insuree.Quote = CalculateQuote(insuree); // <<< dodane
+                db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(insurees);
+            return View(insuree);
         }
+
 
         // GET: Insuree/Edit/5
         public ActionResult Edit(int? id)
@@ -78,16 +80,19 @@ namespace CarInsuranceApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insurees insurees)
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType")] Insurees insuree)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(insurees).State = EntityState.Modified;
+                insuree.Quote = CalculateQuote(insuree); // <<< dodane
+                db.Entry(insuree).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(insurees);
+
+            return View(insuree);
         }
+
 
         // GET: Insuree/Delete/5
         public ActionResult Delete(int? id)
@@ -123,5 +128,46 @@ namespace CarInsuranceApp.Controllers
             }
             base.Dispose(disposing);
         }
+        private decimal CalculateQuote(Insurees insuree)
+        {
+            decimal quote = 50m;
+
+            int age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            if (insuree.DateOfBirth > DateTime.Now.AddYears(-age)) age--;
+
+            if (age <= 18)
+                quote += 100;
+            else if (age >= 19 && age <= 25)
+                quote += 50;
+            else
+                quote += 25;
+
+            if (insuree.CarYear < 2000) quote += 25;
+            if (insuree.CarYear > 2015) quote += 25;
+
+            if (insuree.CarMake.ToLower() == "porsche")
+            {
+                quote += 25;
+                if (insuree.CarModel.ToLower() == "911 carrera")
+                    quote += 25;
+            }
+
+            quote += insuree.SpeedingTickets * 10;
+
+            if (insuree.DUI)
+                quote *= 1.25m;
+
+            if (insuree.CoverageType)
+                quote *= 1.5m;
+
+            return Math.Round(quote, 2);
+        }
+        public ActionResult Admin()
+        {
+            var insurees = db.Insurees.ToList();
+            return View(insurees);
+        }
+
+
     }
 }
